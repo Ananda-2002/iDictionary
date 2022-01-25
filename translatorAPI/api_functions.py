c323fbc5a129ae12d46efc948ab839e06ff74571
Ananda <anandagopaldutta2001@gmail.com>
2022-01-25 16:13:57 +0530

    remove unnessery code and comments

--- translatorAPI/api_functions.py
+++ translatorAPI/api_functions.py
@@ -3,9 +3,6 @@ from gingerit.gingerit import GingerIt
 import nltk
 from googletrans import Translator
 
-# from PyDictionary import PyDictionary
-# dictionary = PyDictionary()
-
 translator = Translator()
 
 parts_of_speech_dict = {
@@ -49,16 +46,6 @@ parts_of_speech_dict = {
 languages_dict = {}
 languages_dict.update({'languages': ['afrikaans', 'albanian', 'amharic', 'arabic', 'armenian', 'azerbaijani', 'basque', 'belarusian', 'bengali', 'bosnian', 'bulgarian', 'catalan', 'cebuano', 'chichewa', 'chinese (simplified)', 'chinese (traditional)', 'corsican', 'croatian', 'czech', 'danish', 'dutch', 'english', 'esperanto', 'estonian', 'filipino', 'finnish', 'french', 'frisian', 'galician', 'georgian', 'german', 'greek', 'gujarati', 'haitian creole', 'hausa', 'hawaiian', 'hebrew', 'hebrew', 'hindi', 'hmong', 'hungarian', 'icelandic', 'igbo', 'indonesian', 'irish', 'italian', 'japanese', 'javanese', 'kannada', 'kazakh', 'khmer',
                       'korean', 'kurdish (kurmanji)', 'kyrgyz', 'lao', 'latin', 'latvian', 'lithuanian', 'luxembourgish', 'macedonian', 'malagasy', 'malay', 'malayalam', 'maltese', 'maori', 'marathi', 'mongolian', 'myanmar (burmese)', 'nepali', 'norwegian', 'odia', 'pashto', 'persian', 'polish', 'portuguese', 'punjabi', 'romanian', 'russian', 'samoan', 'scots gaelic', 'serbian', 'sesotho', 'shona', 'sindhi', 'sinhala', 'slovak', 'slovenian', 'somali', 'spanish', 'sundanese', 'swahili', 'swedish', 'tajik', 'tamil', 'telugu', 'thai', 'turkish', 'ukrainian', 'urdu', 'uyghur', 'uzbek', 'vietnamese', 'welsh', 'xhosa', 'yiddish', 'yoruba', 'zulu']})
-
-
-# def get_definition(word):
-#     try:
-#         definition = (dictionary.meaning(word))
-#         return definition.get("Noun")
-
-#     except Exception as e:
-#         print(e)
-#         return []
 
 
 def get_synonyms(word):
@@ -182,14 +169,12 @@ def get_all(word, form, to):
         syn_ant = get_synonyms(correct_word).get('data')
         part_of_speech_data = get_parts_of_speech(correct_word).get('data')
         translation = get_trans(correct_word, form, to).get('data')
-        # definition = get_definition(correct_word)
         result = {
             "word": word,
             "corrected_word": correct_word,
             "syn_ant": syn_ant,
             "parts_of_speech": part_of_speech_data,
             "translation": translation,
-            # "definition": definition
         }
         return {'data': result}
 
