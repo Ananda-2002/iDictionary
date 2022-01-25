d7ead355c926f475a739be3cb76389eba53e21fb
Ananda <anandagopaldutta2001@gmail.com>
2022-01-25 12:54:32 +0530

    add nltk.txt file

--- translatorAPI/api_functions.py
+++ translatorAPI/api_functions.py
@@ -1,6 +1,6 @@
 from nltk.corpus import wordnet  # Import wordnet from the NLTK
 from gingerit.gingerit import GingerIt
-import nltk
+from nltk import pos_tag, word_tokenize
 from googletrans import Translator
 
 
@@ -122,8 +122,8 @@ def get_correct_word(word):
 def get_parts_of_speech(word):
     try:
         sentence = word
-        tokens = nltk.word_tokenize(sentence)
-        tagged = nltk.pos_tag(tokens)
+        tokens = word_tokenize(sentence)
+        tagged = pos_tag(tokens)
         parts_of_speech = tagged
         result_dict = {}
         for items in parts_of_speech:
